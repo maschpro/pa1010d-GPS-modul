02f64a9b3c9280e048a8c3881e7698cca9266b42
maschpro <150936602+maschpro@users.noreply.github.com>
2024-03-25 15:36:15 +0100

    improved code
    I added some queries and edit the output format for time UTC and position

--- pa1010d.js
+++ pa1010d.js
@@ -5,7 +5,7 @@ const DEVICE_ADDRESS = 0x10; // The I2C address of PA1010D module
 
 const PMTK_STANDBY = 'PMTK161,0*28';
 const PMTK_AWAKE = 'PMTK010,002*2D';
-const PMTK_FULL_COLD_START = '104*37';
+const PMTK_FULL_COLD_START = 'PMTK104*37';
 const PMTK_Q_RELEASE = 'PMTK605*31';
 const PMTK_SET_NMEA_UPDATE_1HZ = 'PMTK220,1000*1F';
 const PMTK_SET_NMEA_OUTPUT_GLLONLY = 'PMTK314,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29'; // turn on only the GPGLL sentence
@@ -24,59 +24,121 @@ const PMTK_API_SET_FIX_CTL_200_MILLIHERTZ = 'PMTK300,5000,0,0,0,0*18'; //Once ev
 
 const i2cBus = i2c.openSync(I2C_BUS_NUMBER);
 const buffer = Buffer.alloc(255); // Adjust buffer size as needed
-//Initialisierung 
 
+//Initialisierung 
+//sendPMTKPacket(PMTK_FULL_COLD_START);
+sendPMTKPacket(PMTK_SET_NMEA_OUTPUT_RMCGGA);
 sendPMTKPacket(PMTK_SET_NMEA_UPDATE_1HZ);
 sendPMTKPacket(PMTK_SET_BAUD_9600);
-sendPMTKPacket(PMTK_SET_NMEA_OUTPUT_RMCGGAGSA);
 sendPMTKPacket(PMTK_API_SET_FIX_CTL_200_MILLIHERTZ);
 
 //Read GPS data periodically
-setInterval(readGPSData, 500); // Read every 1 second
+setInterval(readGPSData, 1000); // Read every 1 second
+
+/*
+i = 0;
+while(i<10) {
+  readGPSData();
+  i++;
+  delay(1000);
+};
+*/
 
 function sendPMTKPacket(set) {
   PMTK_SET = ('$'+set+'\r\n'); //add preamble und <cr><lf>
-  const pmtkset = Buffer.from(PMTK_SET);
+ // console.log(PMTK_SET);
+  pmtkset = Buffer.from(PMTK_SET);
   i2cBus.i2cWrite(DEVICE_ADDRESS, pmtkset.length, pmtkset, (err, bytesWritten, buffer) => {
     if (err) {
       console.error('Error sending data:', err);
     }else{
-     // console.log(PMTK_SET);
-    //  wait4ack();
+   //wait4ack();
     }; //auf Bestätigung warten (PMTK_ACK 'PMTK001,CMD,FLAG') //Flag: 0=Invalid; 1=unsupported; 2=valid, but failed; 3=succeeded
+
   });
 };
 
+  function format(num, length) {
+  num = num.toString();
+  while (num.length < length) num = '0' + num;
+  return num;
+  }
+
 function parse(data) {
-  //console.log(data);
-  while (data.includes('GGA',3)) { //GGA Sentence
-  console.log(data);
-  const splitString = data.split(',');
-  console.log(splitString[2]);
+  let SentenceStart = data.indexOf('$GNGGA');
+  let SentenceEnd = data.indexOf('*',SentenceStart);
+//console.log(SentenceStart, SentenceEnd);
+  let GGASentence = data.slice(SentenceStart,SentenceEnd);
+  let splitString = GGASentence.split(',');
+//console.log(splitString);
 
-  let time = parseInt(splitString[1]);
-  let hour = time /10000;
-  let minute = (time % 10000) /100;
-  let seconds = (time % 100);
-  let milliseconds = data.slice(14,17) * 1000;
-  console.log('Time: ',hour,':',minute,':',seconds,'.',milliseconds);
+  let time = splitString[1];
+  let hour = parseInt(time.substr(0,2));
+  let minute = parseInt(time.substr(2,2));
+  let seconds = parseInt(time.substr(4,2));
+  let milliseconds = time * 1000;
+  
+  var latitude = splitString[2];
+  var latDeg = parseInt(latitude.substr(0,2));
+  var latMin = parseInt(latitude.substr(2,2));
+  var latSek = parseInt(latitude.substr(5,4));
 
-  const latitudeDegrees = parseInt(splitString[3]);
-  const hemisphereLatitude = parseInt(splitString[4]);
-  const longitude = parseInt(splitString[5]);
-  const hemisphereLongitude = parseInt(splitString[6]);
-  const fixquality = parseInt(splitString[7]);
-  if (fixquality > 0) {
-  console.log('Wir haben ein fix') };
-  const satellites = parseInt(splitString[8]);
-  //if (satellites > 0) {
-  console.log('Anzahl der Satelliten:',satellites);
-  const HDOP  = parseInt(splitString[9]);
-  const altitude = parseInt(splitString[10]);
-  const geoidheight = parseInt(splitString[12]);
-    return; 
+  let hemisphereLatitude = (splitString[3]);
+  
+  var longitude = splitString[4];
+  var longDeg = parseInt(longitude.substr(0,3));
+  var longMin = parseInt(longitude.substr(3,2));
+  var longSek = parseInt(longitude.substr(6,4));
+
+  const hemisphereLongitude = (splitString[5]);
+
+  const fixquality = parseInt(splitString[6]);
+  const satellites = parseInt(splitString[7]);
+  const HDOP  = parseFloat(splitString[8]);
+  const altitude = parseFloat(splitString[9]);
+  const geoidheight = parseFloat(splitString[11]);
+
+
+
+  while (fixquality === 0) {
+  console.log('\x1b[31m','Warten auf Fix!','\n');
+  break;
   };
-}
+
+  while (data.includes('PMTK001',5) )  {
+  let SentenceStart = data.indexOf('$PMTK');
+  let SentenceEnd = data.indexOf('*',SentenceStart);
+  let Sentence = data.slice(SentenceStart,SentenceEnd);
+  let splitString = Sentence.split(',');
+  let PacketType = splitString[1];
+  let Flag = parseInt(splitString[2]);
+  switch (Flag) {
+    case 0:
+      var PMTK_ACK = 'invalid';
+      break;
+    case 1:
+      var PMTK_ACK = 'unsopported';
+      break;
+    case 2:
+      var PMTK_ACK = 'valid, but action failed';
+      break;
+    case 3:
+      var PMTK_ACK = 'valid, and action succeeded';
+      break;
+  }
+  console.log('\x1b[32m','Der Befehl:',PacketType,'wurde',PMTK_ACK,' ausgeführt.');
+  break;
+  };
+
+  console.log("\x1b[0m",'Zeit UTC: ',format(hour,2),':',format(minute,2),':',format(seconds,2));
+  console.log('Anzahl der Satelliten:',satellites);
+
+//console.log(longitude);
+
+  console.log('Lat:', format(latDeg,2),'\u00B0',format(latMin,2),'\u0027',format(latSek,4),'\u0027\u0027',hemisphereLatitude);
+  console.log('Long:', format(longDeg,3),'\u00B0',format(longMin,2),'\u0027',format(longSek,4),'\u0027\u0027',hemisphereLongitude,'\n');
+}; 
+
 
 
 function delay(time) {
@@ -88,22 +150,25 @@ function readGPSData() {
     if (err) {
       console.error('Error reading data:', err);
       } else {
-      const data = buffer.toString('utf8', 0, bytesRead);
-      //console.log(data);
-      parse(data);
-    }     
-  });
+      var data = buffer.toString('utf8', 0, bytesRead);
+      var index = data.indexOf("$");
+      if (index !== -1) {
+        parse(data);
+    }else{
+      return;
+      };
+      };
+  }); 
 };
 
 //Bestätigung eines Commands abwarten
 function wait4ack() {
-  i2cBus.readI2cBlockSync(DEVICE_ADDRESS, 0, buffer.length, buffer);
-  const data = buffer.toString('utf8', 0, buffer.length);
-  let count = 1;
+  readGPSData();
   while ( (count < 100) || ( data.includes('001',5) && PMTK_SET.slice(5,8)===data.slice(9,12) && data.slice(13,14)==='3' ) ) {
-  console.log('Versuch:', count);
+   console.log('Versuch:', count);
   console.log('Daten', data);
   count ++;
+  delay(1000);
   };
   /*
   do {
